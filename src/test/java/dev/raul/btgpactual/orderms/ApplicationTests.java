267aaddf4909667a488c412d0648d0f2a6299c70
raulmaciel <raulmacielxd@gmail.com>
2026-03-16 19:28:24 -0300

    first commit

---
+++ src/test/java/dev/raul/btgpactual/orderms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raul.btgpactual.orderms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
