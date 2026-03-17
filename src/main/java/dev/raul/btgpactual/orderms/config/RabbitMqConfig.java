daa8401f2f977b5532121ef2664ec04f90da5a3c
raulmaciel <raulmacielxd@gmail.com>
2026-03-17 06:19:17 -0300

    feat: add RabbitMQ queue configuration

---
+++ src/main/java/dev/raul/btgpactual/orderms/config/RabbitMqConfig.java
@@ -0,0 +1,8 @@
+package dev.raul.btgpactual.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+}
